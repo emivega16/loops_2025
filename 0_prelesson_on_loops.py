d5d81505721701a2cca9b6b4bddac03736ae3876
emivega16 <emivega316@gmail.com>
2025-12-09 14:22:03 -0600

    Refactor comments and add break example in loops
    Updated comments and added a loop example that breaks on 13.

--- 0_prelesson_on_loops.py
+++ 0_prelesson_on_loops.py
@@ -1,2 +1,14 @@
-Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
-While loops  
+# Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
+# If looping is available then that means it's and iteration
+
+# credit_card = "8837-9812-5265-3462"
+
+# for x in credit_card:
+#     print(x)
+
+for x in range(1, 21):
+     if x == 13:
+         break
+     else:
+         print(x)
+
